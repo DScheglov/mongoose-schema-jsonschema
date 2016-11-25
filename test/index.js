9aecd6427668202935b9190a9c111a849b0a241d
Dmitry Scheglov <Dmitry.Scheglov@gmail.com>
2016-11-25 19:32:40 +0200

    v.1.0.9 circular issue fixed

--- test/index.js
+++ test/index.js
@@ -6,3 +6,4 @@ require('./suites/selection');
 require('./suites/description');
 require('./suites/population');
 require('./suites/uploading-mongoose-implicitly');
+require('./suites/circular-refs');
---
+++ test/suites/circular-refs.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var mongoose = require('../../index')(require('mongoose'));
+var assert = require('assert');
+
+
+describe('Circular refs: Schema.jsonSchema()', function() {
+
+  it('should replace schema with $ref to it', function () {
+
+    var mSchema = new mongoose.Schema({
+      title: String
+    });
+    mSchema.add({child: mSchema});
+
+    var jsonSchema = mSchema.jsonSchema();
+
+    assert.deepEqual(jsonSchema, {
+      id: '#schema_1',
+      type: 'object',
+      properties: {
+        title: {type: 'string'},
+        child: {$ref: '#schema_1'},
+        _id: {type: 'string', format: 'uuid', pattern: '^[0-9a-fA-F]{24}$'}
+      }
+    })
+
+  });
+
+});
