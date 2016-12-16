0f44708add9e09328951a2245a5064672154d72b
Dmitry Scheglov <Dmitry.Scheglov@gmail.com>
2016-12-16 14:05:16 +0200

    coveralls fix

--- test.sh
+++ test.sh
@@ -4,6 +4,6 @@ if [ -f ./coveralls.sh ]; then
 	echo coveralls.sh
   ./coveralls.sh
 else
-  NODE_ENV=test istanbul cover _mocha --report lcovonly -- -R spec && cat ./coverage/lcov.info | coveralls && rm -rf ./coverage
+  NODE_ENV=test istanbul cover _mocha --report lcovonly -- -R spec && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js && rm -rf ./coverage
 fi
 
