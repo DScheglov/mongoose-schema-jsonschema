11a29e0c273a2d5a55b6936ee37deb8e10a990c0
Dmitry Scheglov <Dmitry.Scheglov@gmail.com>
2016-12-20 11:52:13 +0200

    ajv support checked

--- test/index.js
+++ test/index.js
@@ -2,6 +2,7 @@
 
 require('./suites/translation');
 require('./suites/validation');
+require('./suites/ajv-validation');
 require('./suites/selection');
 require('./suites/description');
 require('./suites/population');
