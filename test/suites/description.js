6b2ac28e7da54f3a1a67165fee1af3693c61b633
Dmytro Shchehlov <shchehlov.dmytro@pdffiller.team>
2017-05-01 11:05:32 +0300

    [issue#4] description for array-types

--- test/suites/description.js
+++ test/suites/description.js
@@ -85,4 +85,41 @@ describe('Description: Schema.jsonSchema()', function() {
 
   });
 
+  it('should add description for array', function () {
+
+    var mSchema = new mongoose.Schema({
+      name: String,
+      inputs: {
+        type: [String], 
+        index: true, 
+        description: 'Information operated on by rule'
+      },
+      outputs: {
+        type: [String],
+        description: 'Information produced by rule'
+      },
+    });
+
+    var jsonSchema = mSchema.jsonSchema();
+
+    assert.deepEqual(jsonSchema, {
+      type: 'object',
+      properties: {
+        name: { type: 'string' },
+        inputs: { 
+          type: 'array', 
+          items: { type: 'string' },
+          description: 'Information operated on by rule'
+        },
+        outputs: { 
+          type: 'array', 
+          items: { type: 'string' },
+          description: 'Information produced by rule'
+        },
+        _id: { type: 'string', pattern: '^[0-9a-fA-F]{24}$' } 
+      } 
+    })
+
+  });
+
 });
